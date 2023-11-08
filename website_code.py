681de63278fa0931c36354e8bc43b9237a8c7766
dmillerbt <123419800+dmillerbt@users.noreply.github.com>
2023-11-07 22:47:50 -0600

    Update website_code.py

--- website_code.py
+++ website_code.py
@@ -8,6 +8,7 @@ def load_lottie(url):
     return None
   return r.json()
 
+lottie_animation = load_lottie("https://lottie.host/e338d336-1460-44d6-8cd1-cee4ebf70337/SUJkYtg7kK.json")
 st.set_page_config(page_title="My Webpage", page_icon=":hundred_points:",layout="wide")
 
 #Website Header
@@ -20,13 +21,12 @@ with st.container():
 #Who Am I?
 with st.container():
   left_column, right_column = st.columns(2)
-  st.write("---")
-  st.header("Who am I>")
-  st.write("##")
-  st.write("I work at Baker Tilly as a consultant")
+  with left_column:
+    st.write("---")
+    st.header("Who am I>")
+    st.write("##")
+    st.write("I work at Baker Tilly as a consultant")
 
-#Lottie file
-lottie_animation = load_lottie("https://lottie.host/e338d336-1460-44d6-8cd1-cee4ebf70337/SUJkYtg7kK.json")
-with right_column:
-  stl(lottie_animation, height = 250, key = "data_viz")
+  with right_column:
+    stl(lottie_animation, height = 250, key = "data_viz")
 
