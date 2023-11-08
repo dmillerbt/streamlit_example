10dabddd7086422e49ab84e648e1d6a732cb6295
dmillerbt <123419800+dmillerbt@users.noreply.github.com>
2023-11-07 22:33:57 -0600

    Update website_code.py

--- website_code.py
+++ website_code.py
@@ -2,7 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="My Webpage", page_icon=":hundred_points:",layout="wide")
 
-st.subheader("Hi, I am Danny Miller")
-st.title("A Data Scientist in Chicago")
-st.write("I am looking to learn more about web development")
-st.write("[Learn More ->] (https://github.com/dmillerbt/)")
+with st.container():
+  st.subheader("Hi, I am Danny Miller")
+  st.title("A Data Scientist in Chicago")
+  st.write("I am looking to learn more about web development")
+  st.write("[Learn More ->] (https://github.com/dmillerbt/)")
+
+with st.container():
+  st.write("---")
+  st.header("Who am I>")
+  st.write("##")
+  st.write("I work at Baker Tilly as a consultant")
