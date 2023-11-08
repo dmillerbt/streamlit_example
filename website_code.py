9224e6255ce2babf5dd6ddfb4a46cb1779502609
dmillerbt <123419800+dmillerbt@users.noreply.github.com>
2023-11-07 22:29:35 -0600

    Create website_code.py

---
+++ website_code.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":hundred_points:",layout="wide")
+
+st.subheader("Hi, I am Danny Miller")
+st.title("A Data Scientist in Chicago")
+st.write("I am looking to learn more about web development")
+st.write("[Learn More ->] (https://github.com/dmillerbt/)")
