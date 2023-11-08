9a465f8427ca6b2873f2c40485deef7d52bc868a
dmillerbt <123419800+dmillerbt@users.noreply.github.com>
2023-11-07 22:44:51 -0600

    Update website_code.py

--- website_code.py
+++ website_code.py
@@ -19,13 +19,14 @@ with st.container():
 
 #Who Am I?
 with st.container():
+  left_column, right_column = st.columns(2)
   st.write("---")
   st.header("Who am I>")
   st.write("##")
   st.write("I work at Baker Tilly as a consultant")
 
 #Lottie file
-lottie_animation = "https://lottie.host/e338d336-1460-44d6-8cd1-cee4ebf70337/SUJkYtg7kK.json"    
+lottie_animation = load_lottie("https://lottie.host/e338d336-1460-44d6-8cd1-cee4ebf70337/SUJkYtg7kK.json")
 with right_column:
   stl(lottie_animation, height = 250, key = "data_viz")
 
